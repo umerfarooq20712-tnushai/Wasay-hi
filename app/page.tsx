4b77f6ff0d046d1590dab6502d6755b17ea9fa1c
JUST UMER <umerfarooq.20.7.12@gmail.com>
2026-09-26 08:28:22 +0500

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-black text-white">
-      <h1 className="text-4xl font-bold mb-4">TNUSHAI X UMER</h1>
-      <p className="text-xl text-gray-400">Umer-X-Speed is deploying successfully!</p>
-    </main>
-  );
-}
